8a14ce7de81713eaa28b613a6c097af8a7ee4807
nobi16 <ajraichura@gmail.com>
2022-04-19 16:19:46 +0530

    extra files deleted

--- connect-admin/src/reduxstore/business/businessType.js
+++
@@ -1,5 +0,0 @@
-export const sendreq = "sendreq";
-export const getdata = "getdata";
-export const error_occur = "error";
-export const service = "service";
-export const servicepopup = "servicepopup";
